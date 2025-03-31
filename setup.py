ae7fc8f98b355022c58680f1e8c3c563edde6a2a
harry <rogerhoyos80@gmail.com>
2025-03-30 21:30:58 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repositorio-release",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="roger ",                         # Tu nombre
+    author_email="rogerhoyos80@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/roger1717/repositorio-release",     # URL del proyecto
+)
